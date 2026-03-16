8ba3b49224393e488b11a28ea1682d06e69d327d
Abhilash <abhilash.3828@gmail.com>
2026-03-16 19:45:26 +0530

    Remove VALORANT 5v5 tournament entry
    Removed the VALORANT 5v5 tournament entry from the tournaments data.

--- backend/data/tournaments.js
+++ backend/data/tournaments.js
@@ -3,10 +3,5 @@ module.exports = {
     id: "freefire-solo",
     name: "Free Fire Solo Showdown",
     slots: 50
-  },
-  "valorant-5v5": {
-    id: "valorant-5v5",
-    name: "VALORANT 5v5 Clash",
-    slots: 10
   }
 };
