513aa5959bbb2ba133aa7426dc12db6944d33799
Abhilash <abhilash.3828@gmail.com>
2026-03-17 14:11:12 +0530

    Change logo image in Navbar
    Updated logo image in Navbar component.

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -27,7 +27,7 @@ export default function Navbar() {
     >
       <div className="mx-auto flex max-w-6xl items-center justify-between px-4 py-3">
         <Link href="/" className="flex items-center gap-3">
-          <Image src="/logo.svg" alt="NEXX Esports" width={120} height={48} />
+          <Image src="/current-logo.jpg" alt="NEXX Esports" width={140} height={55} />
         </Link>
 
         <nav className="hidden items-center gap-6 text-sm font-semibold tracking-wide text-white lg:flex">
